ecaad0222ef43eb78577867ad966c21c0b6934fe
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2019-10-09 17:05:37 +0200

    Remove 0.11 extra set of list brackets

--- main.tf
+++ main.tf
@@ -108,15 +108,7 @@ resource "aws_security_group_rule" "public" {
   to_port   = var.security_group_public_rules[count.index]["port"]
   protocol  = "tcp"
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
-  cidr_blocks = [var.security_group_public_rules[count.index]["source"]]
+  cidr_blocks = var.security_group_public_rules[count.index]["source"]
 
   security_group_id = aws_security_group.load_balancers.id
 }
